d8641bdbb13ca64719be05b3c81f5275498b0a85
Parwat Kunwar <parwatkunwar08@gmail.com>
2023-07-24 20:28:55 -0400

    Add project and task controller

---
+++ src/controllers/project.controller.js
@@ -0,0 +1,86 @@
+import ProjectModel from "../models/project.model.js";
+
+class ProjectController {
+  constructor() { }
+
+  static async getProject(req, res, next) {
+    try {
+      let projects = await ProjectModel.find();
+
+      res.json({
+        success: true,
+        data: projects
+      });
+    } catch (error) {
+      return res.status(500).json({ error: error.toString() });
+    }
+  }
+
+  static async addProject(req, res, next) {
+    try {
+      const newProject = new ProjectModel(req.body);
+      await newProject.save();
+
+      return res.status(201).json({
+        success: true,
+        data: newProject,
+        message: 'Project created successfully'
+      });
+    } catch (err) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+
+  static async getProject(req, res, next) {
+    try {
+      const { id } = req.params;
+      const project = await ProjectModel.findById(id);
+
+      if (!project) {
+        return res.status(401).json({
+          success: false,
+          message: "Project not found."
+        });
+      } else {
+        return res.status(401).json({
+          success: true,
+          data: project
+        });
+      }
+    } catch (error) {
+      return res.status(500).json({ error: error.toString() });
+    }
+  }
+
+  static async updateProject(req, res, next) {
+    let { data } = req.body;
+    let { id } = req.params;
+
+    try {
+      const updatedProject = await ProjectModel.findByIdAndUpdate(id, data, {
+        new: true, // Return the modified document instead of the original one
+        runValidators: true, // Run Mongoose validators for the updatedData
+      });
+
+      if (!updatedProject) {
+        return res.status(401).json({
+          success: false,
+          message: "Project not found."
+        });
+      } else {
+        return res.status(401).json({
+          success: true,
+          data: updatedProject
+        });
+      }
+    } catch (error) {
+      return res.status(500).json({ error: error.toString() });
+    }
+  }
+
+  static async deleteUser() {
+
+  }
+}
+
+export default ProjectController;
---
+++ src/controllers/task.controller.js
@@ -0,0 +1,86 @@
+import TaskModel from "../models/task.model.js";
+
+class TaskController {
+  constructor() { }
+
+  static async getTasks(req, res, next) {
+    try {
+      let tasks = await TaskModel.find();
+
+      res.json({
+        success: true,
+        data: tasks
+      });
+    } catch (error) {
+      return res.status(500).json({ error: error.toString() });
+    }
+  }
+
+  static async addTask(req, res, next) {
+    try {
+      const newTask = new TaskModel(req.body);
+      await newTask.save();
+
+      return res.status(201).json({
+        success: true,
+        data: newTask,
+        message: 'Task created successfully'
+      });
+    } catch (err) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+
+  static async getTask(req, res, next) {
+    try {
+      const { id } = req.params;
+      const task = await TaskModel.findById(id);
+
+      if (!task) {
+        return res.status(401).json({
+          success: false,
+          message: "Task not found."
+        });
+      } else {
+        return res.status(401).json({
+          success: true,
+          data: task
+        });
+      }
+    } catch (error) {
+      return res.status(500).json({ error: error.toString() });
+    }
+  }
+
+  static async updateTask(req, res, next) {
+    let { data } = req.body;
+    let { id } = req.params;
+
+    try {
+      const updatedTask = await TaskModel.findByIdAndUpdate(id, data, {
+        new: true, // Return the modified document instead of the original one
+        runValidators: true, // Run Mongoose validators for the updatedData
+      });
+
+      if (!updatedTask) {
+        return res.status(401).json({
+          success: false,
+          message: "Task not found."
+        });
+      } else {
+        return res.status(401).json({
+          success: true,
+          data: updatedTask
+        });
+      }
+    } catch (error) {
+      return res.status(500).json({ error: error.toString() });
+    }
+  }
+
+  static async deleteTask() {
+
+  }
+}
+
+export default TaskController;
